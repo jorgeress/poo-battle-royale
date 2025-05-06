805228b77ed6dafb6afe7173984a482f65e466f9
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-07 01:02:50 +0200

    hay una clase extra

--- src/main/main.java
+++ src/main/main.java
@@ -4,10 +4,8 @@ import controller.GameController;
 
 public class main {
 
-	public class Main {
 	    public static void main(String[] args) {
 	        GameController controller = new GameController();
 	        controller.startGame();
 	    }
-	}
 }
