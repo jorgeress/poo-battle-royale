7e0a438d152f61eeecb60d1333b71b805549310e
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-07 22:58:25 +0200

    main to Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 import controller.GameController;
 
-public class main {
+public class Main {
 
 	    public static void main(String[] args) {
 	        GameController controller = new GameController();
