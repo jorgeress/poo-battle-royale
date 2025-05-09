ca9e279a64ab2149fea6aa5a4ca2f827fa7cfb83
jorgeress <162486102+jorgeress@users.noreply.github.com>
2025-05-09 11:33:54 +0200

    main no funcionaba

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 import controller.GameController;
 
 
-	public class main {
+	public class Main {
 	    public static void main(String[] args) {
 	        GameController controller = new GameController();
 	        controller.startGame();
